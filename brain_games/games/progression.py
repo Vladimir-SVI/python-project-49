8f5ca3fcba74bad22fd0e63f7da66270a1aab380
SVI <Svinovoch@yandex.ru>
2024-02-05 15:30:49 +0300

    progression using the range function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,15 +5,12 @@ rules_game = 'What number is missing in the progression?'
 
 def run_game():
     length_progression = 10
-    list_progression = []
     first_element_progression = random.randint(1, 100)
     denominator_progression = random.randint(1, 10)
-    list_progression.append(str(first_element_progression))
-    for i in range(1, length_progression):
-        progression_elements = first_element_progression \
-            + denominator_progression
-        list_progression.append(str(progression_elements))
-        first_element_progression = progression_elements
+    progression = list(range(first_element_progression,
+                       first_element_progression+length_progression*denominator_progression,
+                       denominator_progression))
+    list_progression = list(map(str, progression))
     number_choice = random.choice(list_progression)
     number_index = list_progression.index(number_choice)
     list_progression[number_index] = '..'
