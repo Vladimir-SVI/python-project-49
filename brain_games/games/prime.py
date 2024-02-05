5b6ecce878c939d73beb3bcd90794d2eeb1bce17
SVI <Svinovoch@yandex.ru>
2024-02-05 14:52:10 +0300

    add is_prime function

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,13 +3,18 @@ import random
 rules_game = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def run_game():
-    random_number = random.randint(2, 100)
+def is_prime(number):
     count = 0
-    for i in range(2, random_number // 2 + 1):
-        if (random_number % i == 0):
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
             count = count + 1
     result = 'no'
-    if (count <= 0):
+    if count <= 0:
         result = 'yes'
+    return result
+
+
+def run_game():
+    random_number = random.randint(2, 100)
+    result = is_prime(random_number)
     return {'result': str(result), 'question': random_number}
