f9ee38ffe30e624ac19adc8e0b0dc7e4eec1d2bd
SVI <Svinovoch@yandex.ru>
2024-02-12 15:03:31 +0300

    refactor of function play_round in module even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,5 @@ rules_game = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def play_round():
     question = random.randint(1, 10)
-    result = 'no'
-    if question % 2 == 0:
-        result = 'yes'
+    result = 'yes' if question % 2 == 0 else 'no'
     return question, result
