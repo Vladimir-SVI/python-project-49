105d2b6461034d138705a7fca23a8b6cbe68d2bf
SVI <Svinovoch@yandex.ru>
2024-02-12 15:08:59 +0300

    rename question to random_number

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,6 +4,6 @@ rules_game = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def play_round():
-    question = random.randint(1, 10)
-    result = 'yes' if question % 2 == 0 else 'no'
-    return question, result
+    random_number = random.randint(1, 10)
+    result = 'yes' if random_number % 2 == 0 else 'no'
+    return random_number, result
